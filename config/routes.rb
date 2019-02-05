101ef2202d13b37d77f576108a8b3cc45fef2ca7
Agus Suharto <>
2019-02-05 22:53:29 +0900

    make admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   post 'product/new', to: 'product#new_post'
   get 'product/list', to: 'product#list'
   get 'product/find/barcode/:id', to: 'product#find_barcode_id'
